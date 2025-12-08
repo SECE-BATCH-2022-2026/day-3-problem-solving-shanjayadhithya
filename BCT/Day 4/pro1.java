513064a39e53b8f0874d7c61a9e31be94659ac11
shanjayadhithya <shanjayadhithya.s2022ai-ds@sece.ac.in>
2025-12-08 12:40:22 +0530

    day4

--- BCT/Day 4/pro1.java
+++ BCT/Day 4/pro1.java
@@ -1,28 +1,29 @@
 import java.io.*;
 public class pro1 {
 
-    public static void main(String args[]) throws Exception {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String inp = br.readLine();
+        String s = br.readLine();
         int n = Integer.parseInt(br.readLine());
-        System.out.println(UserMainCode.repeatFront(inp, n));
+        System.out.println(repeatFront(s, n));
     }
-    }
-class UserMainCode{
 
-public static String repeatFront(String s, int n) {
-        if (s.length() == 0) return "";
-
-        if (s.length() < n) {
-            String res = "";
-            for (int i = 0; i < n; i++) res += s;
-            return res;
-        }
-
-        String part = s.substring(0, n);
+    public static String repeatFront(String s, int n) {
         String res = "";
-        for (int i = 0; i < n; i++) res += part;
+        int i = s.length();
+
+        if (i < 3) {
+            for (int j = 0; j < i; j++) {
+                res += s;
+            }
+        } else {
+            String inpl = s.substring(0, n);
+            for (int j = 0; j < n; j++) {
+                res += inpl;
+            }
+        }
 
         return res;
     }
 }
+
